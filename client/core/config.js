bf268586b6556067199829cdd0a3134a04259aee
avallach <github@avallach.ovh>
2020-06-06 22:38:30 +0200

    move dev scripts

--- client/core/config.js
+++ client/core/config.js
@@ -1,6 +1,6 @@
 export default {
-	CLIENT_URL: "http://localhost:5000/client",
-	SERVER_URL: "http://localhost:5000",
+	CLIENT_URL: "https://alpha.themodders.org/client",
+	SERVER_URL: "https://beta.themodders.org",
 	FORUM_NAME: "themodders.org",
 	THEME_COLOR: "#202124",
 	LANG: "pl"
